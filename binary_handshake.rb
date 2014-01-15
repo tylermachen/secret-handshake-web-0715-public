0159c085bd0bc3a8a79dd081abb0fdcdf38be082
Logan Hasson <logan.hasson@gmail.com>
2014-01-15 10:13:07 -0500

    Add rules as comment to top of binary_handshake.rb

--- binary_handshake.rb
+++ binary_handshake.rb
@@ -1,1 +1,9 @@
+# Binary Handshake Rules
+
+# 1 = "wink"
+# 10 = "double blink"
+# 100 = "close your eyes"
+# 1000 = "jump"
+# 10000 = Reverse the order of the operations in the secret handshake.
+
 # Write your code here.
